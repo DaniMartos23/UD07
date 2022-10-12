7cd73211b06a68ecf575dce40b36ac15c31dddeb
DaniMartos23 <dani.martos69@gmail.com>
2022-10-12 16:52:00 +0200

    ejercicio2 acabado

--- src/ej2/Ejercicio2App.java
+++ src/ej2/Ejercicio2App.java
@@ -6,38 +6,49 @@ public class Ejercicio2App {
 	final static double IVAMAX=0.21;
 	final static double IVAMIN=0.04;
 	public static void main(String[] args) {
-		
+		Scanner sc=new Scanner(System.in);
 		Hashtable<String,double[]> productos  = new Hashtable<String,double[]>();
 		productos=crearDict();
 		
 		ArrayList<String> lista= new ArrayList<>();
-		lista=pedirProductos(productos);
-		imprimeProductos(productos,lista);
-		
+		lista=pedirProductos(productos,sc);
+		imprimeProductos(productos,lista,sc);
+		sc.close();
 
 	}
-	private static void imprimeProductos(Hashtable<String, double[]> productos, ArrayList<String> lista) {
+	private static void imprimeProductos(Hashtable<String, double[]> productos, ArrayList<String> lista, Scanner sc) {
 		// funcion que imprime la lista de productos, el IVA aplicado, precio bruto, precio+IVA, numero de articulos comprados
 		// cantidad pagada y cambio a devolver
 		double precio;
-		double total;
+		double total = 0;
 		String nombre;
-		double precio_IVA
-		int cantidad;
-		System.out.println("Articulo \t Numero Articulos \t precio Bruto \t IVA aplicado \t precio+IVA \t");
+		double precio_IVA;
+		int cantidad,cantidadt=0;
+		System.out.printf("%12s %12s %12s %12s %12s\n","Articulo"," Numero Articulos","precio Bruto","IVA aplicado","precio+IVA");
 		for (String objetos:lista) {
 			nombre=objetos.split(",")[0];
 			cantidad=Integer.parseInt(objetos.split(",")[1]);
-			precio=productos.get(nombre)[0];
+			cantidadt+=cantidad;
+			precio=productos.get(nombre)[0]*cantidad;
 			precio_IVA = precio+(productos.get(nombre)[1]*precio);
-			System.out.println(objetos.split(",")+" \t ");
+			total+=precio_IVA;
+			System.out.printf("%12s %12s %12s %12s %12.2f\n",nombre,cantidad,precio,((productos.get(nombre)[1])*100),precio_IVA);
 		}
+		System.out.printf("%12s %12s %38.2f\n", "Total",cantidadt,total);
+		double cantidadp;
+		boolean comp=false;
+		do {
+			System.out.print("Entra cantidad pagada: ");
+			cantidadp=Double.parseDouble(sc.nextLine());
+			if(cantidadp<total) System.out.println("Valor menor al importe total. Vuelve a introducir cantidad...");
+			else comp=true;
+		}while(!comp);
+		System.out.printf("El cambio sera de %.2f€\n",(cantidadp-total));
 		
 		
 	}
-	private static ArrayList<String> pedirProductos(Hashtable<String, double[]> productos) {
+	private static ArrayList<String> pedirProductos(Hashtable<String, double[]> productos, Scanner sc) {
 		// funcion que retorna una array list con el nombre de los productos añadidos
-		Scanner sc= new Scanner(System.in);
 		ArrayList<String> nombres = new ArrayList<>();
 		String producto;
 		Boolean comp;
